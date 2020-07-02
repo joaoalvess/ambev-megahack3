b6074b0826327a988d63dbaec0ea9349f936bbff
Elcoss <jp14700@gmail.com>
2020-07-01 23:03:46 -0300

    project start

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+const login: React.FC = () => {
+  return (
+    <Container>
+      <Title>ola com ovai</Title>
+    </Container>
+  );
+}
+
+export default login;
---
+++ src/pages/login/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1
+`;
+
+export const Title = styled.Text`
+  color: #606;
+  font-size: 20px
+`;
