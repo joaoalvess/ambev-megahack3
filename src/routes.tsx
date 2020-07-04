f5c9967b28b92d7d7f8d529ba16e626949c03f65
Elcoss <jp14700@gmail.com>
2020-07-03 23:53:57 -0300

    update Routes

---
+++ src/routes.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignInSelectAccType from './pages/SignInSelectAccType'
+import SignInClient from './pages/SignInClient'
+import SignInPoint from './pages/SignInPoint'
+import Dashboard from './pages/Dashboard'
+
+const AppStack = createStackNavigator()
+
+const Routes = () => {
+  return(
+    <NavigationContainer>
+      <AppStack.Navigator 
+        headerMode="none"
+        screenOptions={{
+          cardStyle: {
+            backgroundColor: '#d5d5d5'
+          }
+        }}
+      >
+        <AppStack.Screen name="Home" component={Home} />
+        <AppStack.Screen name="Login" component={Login} />
+        <AppStack.Screen name="SignInSelectAccType" component={SignInSelectAccType} />
+        <AppStack.Screen name="SignInClient" component={SignInClient} />
+        <AppStack.Screen name="SignInPoint" component={SignInPoint} />
+        <AppStack.Screen name="Dashboard" component={Dashboard} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default Routes
