9173066d1e3a74459a1bb0ef45884f03f077a484
Elcoss <jp14700@gmail.com>
2020-07-03 23:54:51 -0300

    add status bar, apploading and Roboto Font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { AppLoading } from 'expo'
+import { StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_500Medium, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
 
-import Home from "./src/pages/login/index"
+import Routes from "./src/routes"
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular, 
+    Roboto_500Medium,
+    Roboto_700Bold 
+  })
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <Home />
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <Routes />
+    </>
   );
 }
