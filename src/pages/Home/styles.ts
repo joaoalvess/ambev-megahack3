d6af909794396a3d2b8ec76df99fd476e9f0b849
Elcoss <jp14700@gmail.com>
2020-07-04 00:49:39 -0300

    add Home Page

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+
+export const ViewButton = styled.View`
+  margin-top: 500px;
+`;
