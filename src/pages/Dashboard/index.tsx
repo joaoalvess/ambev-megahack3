0250bcdc2ee17bbd93285f0360e07e94a9977a61
Elcoss <jp14700@gmail.com>
2020-07-05 04:57:27 -0300

    Dashboard map add

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 // import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
-    <View></View>
+    <View style={styles.mapContainer}>
+      <MapView style={styles.map} />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 16,
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+})
 
 export default Dashboard;
